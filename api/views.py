46380cb52c545058e6d7df24af2d211cb995b790
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-02 16:44:17 -0400

    Added default endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(["GET"])
+def getStart(request):
+    r = {"sentence":"Hello world"}
+
+    return Response(r)
