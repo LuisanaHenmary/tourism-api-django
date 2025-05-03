e5e6803bb94e2e149ec264767d3cba7ef2494709
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-03 11:54:43 -0400

    Added get one category view

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from api.models import Category
 from api.serializers import CategotySerializer
+from rest_framework import status
 
 @api_view(["GET"])
 def getStart(request):
@@ -12,5 +13,13 @@ def getStart(request):
 def getAllCategories(request):
     categories = Category.objects.all()
     serializerCategories = CategotySerializer(categories, many=True)
-
     return Response(serializerCategories.data)
+
+@api_view(["GET"])
+def getOneCategory(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+        serializerCategory = CategotySerializer(category)
+        return Response(serializerCategory.data)
+    except Category.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
