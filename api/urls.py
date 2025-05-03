0ef2c4a2ea1f2af4d74976716ce9209cf9291ab0
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-03 15:57:16 -0400

    Added  locations endpoints to urls

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from api.views import getStart, getAllCategories, getOneCategory
+from api.views import (
+    getStart,
+    getAllCategories,
+    getOneCategory,
+    getAllLocations,
+    getOneLocation
+ )
 
 urlpatterns = [
     path('', getStart),
     path('categories', getAllCategories),
-    path('categories/<int:pk>/', getOneCategory),
+    path('categories/<int:pk>', getOneCategory),
+    path('locations', getAllLocations),
+    path('locations/<int:pk>', getOneLocation),
 ]
