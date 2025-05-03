f7aea4a22ba76a3e3920655aaa7c40418b06241c
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-03 11:41:55 -0400

    Added get all categories view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from api.models import Category
+from api.serializers import CategotySerializer
 
 @api_view(["GET"])
 def getStart(request):
     r = {"sentence":"Hello world"}
-
     return Response(r)
+
+@api_view(["GET"])
+def getAllCategories(request):
+    categories = Category.objects.all()
+    serializerCategories = CategotySerializer(categories, many=True)
+
+    return Response(serializerCategories.data)
