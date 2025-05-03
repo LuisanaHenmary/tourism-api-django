2604336ba8a89bff3fead660725efe9ca1769045
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-03 15:40:26 -0400

    Added LocationSerializer serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from api.models import Category
+from api.models import Category, Location
 
 class CategotySerializer(serializers.ModelSerializer):
     class Meta:
         model=Category
+        fields="__all__"
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Location
         fields="__all__"
