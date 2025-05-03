464bf0a156d924e24aea82d115e65091e012e044
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-03 11:56:53 -0400

    Added categories endpoints to urls

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from api.views import getStart
+from api.views import getStart, getAllCategories, getOneCategory
 
 urlpatterns = [
     path('', getStart),
+    path('categories', getAllCategories),
+    path('categories/<int:pk>/', getOneCategory),
 ]
