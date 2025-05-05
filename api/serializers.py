328aad9b7455bd7209a6b1b9c3b69f4624c2aa22
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-05 14:58:51 -0400

    Added ReviewSerialiser serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Category, Location
+from api.models import Category, Location, User, Review
 
 class CategotySerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,15 @@ class CategotySerializer(serializers.ModelSerializer):
 class LocationSerializer(serializers.ModelSerializer):
     class Meta:
         model=Location
+        fields="__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=["id"]
+
+
+class ReviewSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model=Review
         fields="__all__"
