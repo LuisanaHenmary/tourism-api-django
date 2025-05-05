52604144624dc7684fd223f9002c767c6ebe5a92
LuisanaHenmary <luisanahenmaryp@gmail.com>
2025-05-05 15:03:53 -0400

    Added authentication endpoints and post a review url

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,21 @@ from api.views import (
     getAllCategories,
     getOneCategory,
     getAllLocations,
-    getOneLocation
+    getOneLocation,
+    addReview
  )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('', getStart),
-    path('categories', getAllCategories),
-    path('categories/<int:pk>', getOneCategory),
-    path('locations', getAllLocations),
-    path('locations/<int:pk>', getOneLocation),
+    path('categories/', getAllCategories),
+    path('categories/<int:pk>/', getOneCategory),
+    path('locations/', getAllLocations),
+    path('locations/<int:pk>/', getOneLocation),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('reviews/', addReview),
 ]
